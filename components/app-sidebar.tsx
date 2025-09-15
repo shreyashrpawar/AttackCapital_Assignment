4ccc868021517062cda33c482d4fee9af32bad16
Shreyash Pawar <shreyashrpawar28@gmail.com>
2025-09-15 15:30:54 +0530

    Modifications over the template

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,130 @@
+import * as React from "react";
+
+import { SearchForm } from "@/components/search-form";
+import { TeamSwitcher } from "@/components/team-switcher";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import {
+  RiScanLine,
+  RiBardLine,
+  RiUserFollowLine,
+  RiCodeSSlashLine,
+  RiLoginCircleLine,
+  RiLayoutLeftLine,
+  RiSettings3Line,
+  RiLeafLine,
+  RiLogoutBoxLine,
+} from "@remixicon/react";
+
+// This is sample data.
+const data = {
+  teams: [
+    {
+      name: "Attack Capital",
+      logo: "https://attack.capital/images/logo.png",
+    }
+  ],
+  navMain: [
+    {
+      title: "Sections",
+      url: "#",
+      items: [
+        {
+          title: "Dashboard",
+          url: "#",
+          icon: RiScanLine,
+        },
+        {
+          title: "Insights",
+          url: "#",
+          icon: RiBardLine,
+        },
+        {
+          title: "Contacts",
+          url: "#",
+          icon: RiUserFollowLine,
+          isActive: true,
+        },
+        {
+          title: "Tools",
+          url: "#",
+          icon: RiCodeSSlashLine,
+        },
+        {
+          title: "Integration",
+          url: "#",
+          icon: RiLoginCircleLine,
+        },
+        {
+          title: "Layouts",
+          url: "#",
+          icon: RiLayoutLeftLine,
+        },
+        {
+          title: "Reports",
+          url: "#",
+          icon: RiLeafLine,
+        },
+      ],
+    },
+  ],
+};
+
+export default function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+        <hr className="border-t border-border mx-2 -mt-px" />
+        {/* <SearchForm className="mt-3" /> */}
+      </SidebarHeader>
+      <SidebarContent>
+        {/* We create a SidebarGroup for each parent. */}
+        {data.navMain.map((item) => (
+          <SidebarGroup key={item.title}>
+            <SidebarGroupLabel className="uppercase text-muted-foreground/60">
+              {item.title}
+            </SidebarGroupLabel>
+            <SidebarGroupContent className="px-2">
+              <SidebarMenu>
+                {item.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton
+                      asChild
+                      className="group/menu-button font-medium gap-3 h-9 rounded-md bg-gradient-to-r hover:bg-transparent hover:from-sidebar-accent hover:to-sidebar-accent/40 data-[active=true]:from-primary/20 data-[active=true]:to-primary/5 [&>svg]:size-auto"
+                      isActive={item.isActive}
+                    >
+                      <a href={item.url}>
+                        {item.icon && (
+                          <item.icon
+                            className=""
+                            size={22}
+                            aria-hidden="true"
+                          />
+                        )}
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+
+      <SidebarRail />
+    </Sidebar>
+  );
+}
